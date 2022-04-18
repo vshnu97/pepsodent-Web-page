298f98bf0392d0b16ae0092223f95345070a4568
VISHNU K <100349641+vshnu97@users.noreply.github.com>
2022-04-18 09:29:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickme(){
+    alert("Please wait: Sign up page loading")
+    window.location=("https://www.pepsodent.in/sign-up.html")
+}
+function chatme(){
+    alert(" Let's chat with DigiDendist ")
+    window.location=("https://www.pepsodent.in/digidentist.html")
+
+}
